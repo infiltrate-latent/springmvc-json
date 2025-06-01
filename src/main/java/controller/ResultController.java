176f6823108e3fc719a5a68617446d2edeca83e1
consist_surrogate <consist_surrogate@163.com>
2025-06-01 12:26:03 +0800

    spring-webmvc：5.1.7.RELEASE，slf4j-log4j12可以打印日志

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -2,11 +2,16 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
 @Controller
 public class ResultController {
+//    private static final Logger logger = LogManager.getLogger(ResultController.class);
+
     @RequestMapping("/test03-1")
     @ResponseBody
     public String test031() {
+//        logger.info("Processing request to say hello");
         return "result";
     }
 }
