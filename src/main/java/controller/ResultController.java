80e9d70a73adb5fe4d97c9c59a0dd70e324d8f14
intuitive-conscience <consist_surrogate@163.com>
2025-06-03 14:53:26 +0800

    log4j:1.2.17, commons-logging:1.2, spring-webmvc:5.1.7.RELEASE 可以打印日志

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 //import org.apache.logging.log4j.Logger;
-//import org.apache.logging.log4j.LogManager;
+////import org.apache.logging.log4j.LogManager;
 @Controller
 public class ResultController {
 //    private static final Logger logger = LogManager.getLogger(ResultController.class);
