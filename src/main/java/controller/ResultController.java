09f978d1e127ca8bbc6e7b53ed2a36100e2f024b
intuitive-conscience <consist_surrogate@163.com>
2025-06-04 10:12:33 +0800

    spring-webmvc:4.1.1.RELEASE,但因为spring-core(4.1.1.RELEASE.)依赖了commons-logging的，我又在pom文件exclude commons-logging了,所以controller会报错，表明确实是successfully exclude commons-logging。

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -4,14 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 //import org.apache.logging.log4j.Logger;
 ////import org.apache.logging.log4j.LogManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 @Controller
 public class ResultController {
 //    private static final Logger logger = LogManager.getLogger(ResultController.class);
-
+private static Log logger = LogFactory.getLog(ResultController.class);
     @RequestMapping("/test03-1")
     @ResponseBody
     public String test031() {
-//        logger.info("Processing request to say hello");
+        logger.info("Processing request to say hello");
         return "result";
     }
 }
